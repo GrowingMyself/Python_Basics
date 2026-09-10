0382586111d19bda361e42586ae58cc3e559ec15
Prachi <morep4062@gmail.com>
2026-09-10 19:30:34 +0530

    Add files via upload

---
+++ functionbasics.py
@@ -0,0 +1,50 @@
+              # *Functions in python.
+#We use a keyword "def" to define a function
+# syntax- def function_name():
+
+#1.write a function named welcom_message that prints "welcome to python programming!" three times
+
+def welcome_message():
+    print("Welcome to python programming....!")
+welcome_message()
+welcome_message()
+welcome_message()
+
+
+#2.Define a function inspire() thats prints a motivational quote
+
+def inspire():
+    print("you are the master of tour disney")
+inspire()
+
+
+#3.create a function good_morning() that prints "good morning! prachi" call a twice
+
+def good_morning():
+    print("Good_morning!Prachi")
+good_morning()
+good_morning()
+ 
+# write a function show_age(name,age) that prints:prachi is 19 year old
+
+def show_age(name="Prachi",age=19):
+      print(f"{name} is {19} year old")
+show_age("Prachi",19)
+
+#Create a function add_numbers(a,b)that prints both sum and difference
+
+def add_numbers(a=87,b=65):
+    print("sum of two numbers:",a+b)
+    print("difference of two numbers:",a-b)
+
+add_numbers(87,65)
+
+#write a function favorite food(food) that prings Prachi loves< food>
+
+def favorite_food(food):
+    print("Prachi loves",food)
+favorite_food("Gulabjamun")
+
+
+
+
